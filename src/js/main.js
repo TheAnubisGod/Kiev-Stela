35107fd03b0dae583e32c41ef22bc3b6b030b689
TheAnubisGod <vladdiuzhev@gmail.com>
2020-07-18 18:11:29 +0300

    Menu remastered

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $(this).toggleClass('block-inactive');
     });
 
-    $(document).mouseup(function (e){ // событие клика по веб-документу
+    $(document).mouseup(function (e) { // событие клика по веб-документу
         if (!$(".block-wrap").is(e.target) // если клик был не по нашему блоку
             && $(".block-wrap").has(e.target).length === 0) { // и не по его дочерним элементам
             $(".block-wrap").addClass("block-inactive")
@@ -18,10 +18,13 @@ $(document).ready(function () {
             $(".main-header-mobile-content").slideDown()
             document.querySelector("body").style.overflow = "hidden"
             document.querySelector(".top-horizon").style.display = "block"
+            console.log('lol')
+            document.querySelector(".main-arrows").style.top = ((1 - 50 / document.documentElement.clientHeight)*100).toString()+"%"
         } else {
             $(".main-header-mobile-content").slideUp()
             document.querySelector("body").style.overflow = "auto"
             document.querySelector(".top-horizon").style.display = "none"
+            document.querySelector(".main-arrows").style.top = "0"
         }
         $(".main-arrow-left").toggleClass("rotate180")
         $(".main-arrow-right").toggleClass("rotate-180")
